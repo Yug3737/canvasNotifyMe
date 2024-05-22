a25d12b98a73d81f9a21ec6ba69233514bc4cab7
Yug Patel <118321265+Yug3737@users.noreply.github.com>
2024-05-21 22:08:08 -0400

    Added test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+// 
+// file: test.js
+// author: Yug Patel
+// last modiified: 21 May 2024
+//
+
+const express = require('express');
+const app = express();
+
+app.get('/api/v1/courses', (req,res)=>{
+	
+
+})
