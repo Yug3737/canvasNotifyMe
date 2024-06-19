8e8471761dc30f2a145506c830fcf4eec570c99a
Yug Patel <118321265+Yug3737@users.noreply.github.com>
2024-06-18 23:01:23 -0400

    add functionaltiy to view assignment due dates for research writing course

--- canvas_api.py
+++ canvas_api.py
@@ -5,6 +5,8 @@
 #
 
 import requests
+from datetime import datetime
+from dateutil import parser
 
 CANVAS_BASE_URL = 'https://kent.instructure.com/api/v1'
 # Yug's access token
@@ -22,7 +24,7 @@ def get_user_profile():
     return response.json()
 
 def get_user_courses(user_id):
-    url = f"{CANVAS_BASE_URL}/users/{user_id}/courses"
+    url = f"{CANVAS_BASE_URL}/users/{user_id}/courses?enrollment_state=active,invited_or_pending&per_page=100"
     response = requests.get(url, headers=get_headers())
     response.raise_for_status()
     return response.json()
@@ -52,29 +54,53 @@ if __name__ == "__main__":
     print(test_sortable_name)
     print(test_lti_user_id)
 
-    test_email_id = None
-    print(test_email_id)
+    # test_email_id = None
 
     for item in test_text_number_obj:
         if item['type'] == 'email':
             test_email_id= item['address']
             break
+    print(test_email_id)
     
-    # phone_numbers = []
-    # for channel in test_text_number_obj:
-        # if channel['type'] in ['sms', 'phone']:
-            # phone_numbers.append(channel['address'])
-    # print(phone_numbers) 
-
     print("---------------------------------------------------------")
     test_courses = get_user_courses(test_id)
 
     test_course_ids = []
     test_course_names = []
-    for course in test_courses:
-       test_course_ids.append(course['id'])
-       test_course_names.append(course['name']) 
     
-    print(test_course_names)
     print(test_course_ids)
-    # print(test_courses)
+
+    problem_courses = []
+    for test_course in test_courses:
+        # print(test_course)
+        if 'name' in test_course:
+            print(test_course['name'])
+            if "RESEARCH WRITING" in test_course['name']:
+                rw_course = test_course
+        else:
+            print("Key 'name' not found in test_course")
+            problem_courses.append(test_course)
+
+print(problem_courses)
+print(rw_course)
+
+# Now have all the detials for research writing course
+def get_assignments_for_user(course_id):
+    url = f"{CANVAS_BASE_URL}/courses/{course_id}/assignments/"
+    response = requests.get(url, headers=get_headers())
+    response.raise_for_status()
+    return response.json()
+
+rw_course_id = rw_course['id']
+rw_assignments = get_assignments_for_user(rw_course_id)
+# print(rw_assignments)
+print(len(rw_assignments))
+
+rw_hw_duedates = []
+for rw_assignment in rw_assignments:
+    rw_hw_duedates.append(rw_assignment['due_at'])
+    print(rw_assignment['due_at'])
+print(len(rw_hw_duedates))
+
+
+# current_time = 
