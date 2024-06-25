7d7851784da7ed8d2056c45ca44dd73d4472eb4a
Yug Patel <118321265+Yug3737@users.noreply.github.com>
2024-06-24 22:54:28 -0400

    remove bug from compare date_time obj function

--- canvas_api.py
+++ canvas_api.py
@@ -75,14 +75,14 @@ if __name__ == "__main__":
         # print(test_course)
         if 'name' in test_course:
             print(test_course['name'])
-            if "RESEARCH WRITING" in test_course['name']:
+            if "INTERMEDIATE MACROECONOMIC" in test_course['name']:
                 rw_course = test_course
         else:
             print("Key 'name' not found in test_course")
             problem_courses.append(test_course)
 
-print(problem_courses)
-print(rw_course)
+# print(problem_courses)
+# print(rw_course)
 
 # Now have all the detials for research writing course
 def get_assignments_for_user(course_id):
@@ -94,7 +94,7 @@ def get_assignments_for_user(course_id):
 rw_course_id = rw_course['id']
 rw_assignments = get_assignments_for_user(rw_course_id)
 # print(rw_assignments)
-print(len(rw_assignments))
+# print(len(rw_assignments))
 
 rw_hw_duedates = []
 rw_hw_dict = {} # dict with key as HW names and value as due datetime
@@ -104,13 +104,15 @@ for rw_assignment in rw_assignments:
 
 print(rw_hw_dict)
 
-def compare_date_time_obj_iso(obj1, obj2):
+def compare_date_time_obj_iso(obj1:str, obj2:str) -> str:
     obj1_date = obj1.split('T')[0]
     obj2_date = obj2.split('T')[0]
     obj1_time = obj1.split('T')[1]
     obj2_time = obj2.split('T')[1]
     # print(obj2_time)
     # print(obj1_time)
+    # print(obj2_date)
+    # print(obj1_date)
 
     result1 = f"{obj1} is before {obj2}"
     result2 = f"{obj1} is after {obj2}"
@@ -120,11 +122,11 @@ def compare_date_time_obj_iso(obj1, obj2):
     # print("obj1_date", obj1_date)
     obj2_year = int(obj2_date.split('-')[0])
     obj2_month = int(obj2_date.split('-')[1])
-    obj2_date = int(obj2_date.split('-')[2])
+    obj2_day = int(obj2_date.split('-')[2])
     # print(obj2_year) 
     obj1_year = int(obj1_date.split('-')[0])
     obj1_month = int(obj1_date.split('-')[1])
-    obj1_date = int(obj1_date.split('-')[2])
+    obj1_day = int(obj1_date.split('-')[2])
 
     # print(type(obj2_month))
     # print(obj2_month, obj2_date)
@@ -133,16 +135,16 @@ def compare_date_time_obj_iso(obj1, obj2):
     elif obj1_year < obj2_year:
         return result1 
     elif obj1_year == obj2_year:
-        if obj1_month > obj2_date:
+        if obj1_month > obj2_month:
             return result2
         elif obj1_month < obj2_month:
             return result1
         elif obj2_month == obj1_month:
-            if obj1_date > obj2_date:
+            if obj1_day > obj2_day:
                 return result2
-            elif obj1_date < obj2_date:
+            elif obj1_day < obj2_day:
                 return result1
-            elif obj1_date == obj2_date:
+            elif obj1_day  == obj2_day:
                 
                 obj1_hour = obj1_time.split(':')[0]
                 obj2_hour = obj2_time.split(':')[0]
@@ -170,15 +172,45 @@ def compare_date_time_obj_iso(obj1, obj2):
                         return result3
                     
 
-
 rw_hw_due_dates = list(rw_hw_dict.values())
 print(rw_hw_due_dates)
 print("result of comparing 2 datetime strings ->")
 print(rw_hw_due_dates[0])
-print(type(rw_hw_due_dates[0]))
 print(rw_hw_due_dates[1])
-print(type(rw_hw_due_dates[1]))
 print(compare_date_time_obj_iso(rw_hw_due_dates[0], rw_hw_due_dates[1]))
-# print(compare_date_time_obj_iso("2023-06-21T12:30:45", "2023-06-21T12:30:45"))  # Output: "2023-06-21T12:30:45 is the same as 2023-06-21T12:30:45"
-# print(compare_date_time_obj_iso("2023-06-21T12:30:45", "2023-06-21T12:30:46"))  # Output: "2023-06-21T12:30:45 is before 2023-06-21T12:30:46"
-# print(compare_date_time_obj_iso("2023-06-21T12:30:46", "2023-06-21T12:30:45"))  # Output: "2023-06-21T12:30:46 is after 2023-06-21T12:30:45"
+
+# Finding latest current time object
+# precondition: given the OS is working
+# postcondition: returns a string represented in the format "YYYY-MM-DDTDD-MM-SSz"
+def get_current_time_string()-> str:
+    curr_time_obj = datetime.now()
+    time_string = curr_time_obj.strftime('%Y-%m-%dT%H:%M:%S')
+    return time_string
+
+# precondition: given 2 strings one is due date of a HW and ther other is current time,
+#               both in the formats of "YYYY-MM-DDTDD-MM-SSz"
+# postcondition: returns the approximate difference between the date and hours of the two dates 
+#                as an int
+def get_approximate_duedate_difference(duedate: str, today: str) -> int:
+    duedate_day = duedate.split('T')[0].split('-')[2]
+    current_day = today.split('T')[0].split('-')[2]
+    duedate_hour = duedate.split('T')[1].split(':')[0]
+    current_hour = today.split('T')[1].split(':')[0]
+
+    # print(current_hour)
+    # print(duedate_hour)
+    if int(duedate_day) >
+
+get_approximate_duedate_difference('2024-01-30T14:25:00Z' ,'2024-02-08T14:25:00Z')
+
+# precondition: parameters are current time string and homeworks dictionary with keys are homework names and 
+#               values are corresponding hw names
+# postcondition: returns a filtered dictionary whose homework due dates are within 14 days of current date 
+def get_hws_due(hw_dict: dict, current_time: str) -> dict:
+    result_dict = {}
+    for hw, due_date in hw_dict.items():
+        if due_date > current_time and \
+        get_approximate_duedate_difference(due_date, current_time) > 13 and \
+        get_approximate_duedate_difference(due_date, current_time) < 0:
+            result_dict[hw] = due_date
+    return result_dict
