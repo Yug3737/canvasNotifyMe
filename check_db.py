56b372faf2a748dc11a3004c470767943d4214ae
Yug Patel <118321265+Yug3737@users.noreply.github.com>
2024-07-03 14:39:23 -0400

    Add carrier dropdown

--- check_db.py
+++ check_db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 def check_db():
     print("Inside check_db() func")
@@ -8,6 +9,20 @@ def check_db():
         rows = c.fetchall()
         for row in rows:
             print(row)
+
+# conn = sqlite3.connect("test_databases")
+# cursor_obj = conn.cursor()
+
+# table = ''' create table IF NOT EXISTS instructor(ID INTEGER PRIMARY KEY NOT NULL, FNAME VARCHAR(20), LNAME VARCHAR(20), CITY VARCHAR(20), CCODE CHAR(2));'''
+
+# cursor_obj.execute(table)
+
+# print("table is ready")
+
+# cursor_obj.execute('''insert into INSTRUCTOR values (1, 'Rav', 'Ahuja', 'TORONTO', 'CA')''')
+# cursor_obj.execute('''insert into INSTRUCTOR values (2, 'Raul', 'Chong', 'Markham', 'CA'), (3, 'Hima', 'Vasudevan', 'Chicago', 'US')''')
+
+# conn.commit()
 
 if __name__ == "__main__":
     check_db()
