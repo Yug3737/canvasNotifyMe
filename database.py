65bb7588a57d7957564ce89b116ae6ebc861ab93
Yug Patel <118321265+Yug3737@users.noreply.github.com>
2024-07-10 18:45:10 -0400

    Add init_ap(app) func in check_db.py

--- database.py
+++ database.py
@@ -9,3 +9,6 @@ Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+if __name__ == '__main__':
+    pass
--- smsBot/hello.py
+++ smsBot/hello.py
@@ -1,10 +1,12 @@
 import smtplib
 from email.message import EmailMessage
-from emailtoSMSConfig import senderEmail, gatewayAddress, appKey
+from emailtoSMSConfig import senderEmail, appKey
+
 
 msg = EmailMessage()
 msg.set_content("\n Here would be your Canvas assignments that would be due within the next 14 days.")
 
+gatewayAddress = "2166324947@vtext.com"
 msg['From'] = senderEmail # 'email@address.com'
 msg['To'] = gatewayAddress #
 msg['Subject'] = 'Hello from canvasNotifyMe!'
