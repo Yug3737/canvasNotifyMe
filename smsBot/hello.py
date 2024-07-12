66d5b1416ed4deddd68573ca72ff40ada71d0ca5
Yug Patel <118321265+Yug3737@users.noreply.github.com>
2024-07-12 18:31:15 -0400

    Separated retrieval of gatewayAddress

--- smsBot/hello.py
+++ smsBot/hello.py
@@ -1,12 +1,17 @@
 import smtplib
 from email.message import EmailMessage
 from emailtoSMSConfig import senderEmail, appKey
+import os
+import sys
+import importlib.util
 
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, parent_dir)
+from app import gatewayAddress 
 
 msg = EmailMessage()
 msg.set_content("\n Here would be your Canvas assignments that would be due within the next 14 days.")
 
-gatewayAddress = "2166324947@vtext.com"
 msg['From'] = senderEmail # 'email@address.com'
 msg['To'] = gatewayAddress #
 msg['Subject'] = 'Hello from canvasNotifyMe!'
